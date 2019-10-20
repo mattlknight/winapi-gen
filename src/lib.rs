0d27cc63712aeca16942d7365e7608a84ec1b6dc
Matthew Knight <mattlknight@gmail.com>
2019-10-20 10:22:06 -0500

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,39 @@
-#[macro_use] extern crate lalrpop_util;
+#[macro_use] extern crate failure_derive;
+use lalrpop_util::lalrpop_mod;
+
 pub mod ast;
+mod header_file;
+pub use header_file::HeaderFile;
 
-lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
-lalrpop_mod!(pub calculator6);
-
-#[test]
-fn calculator1() {
-    assert!(calculator1::TermParser::new().parse("22").is_ok());
-    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator1::TermParser::new().parse("((22)").is_err());
-}
+lalrpop_mod!(pub header);
 
 #[test]
-fn calculator6() {
-    let mut errors = Vec::new();
-
-    let expr = calculator6::ExprsParser::new()
-        .parse(&mut errors, "22 * + 3")
-        .unwrap();
-    assert_eq!(&format!("{:?}", expr), "[((22 * error) + 3)]");
-
-    let expr = calculator6::ExprsParser::new()
-        .parse(&mut errors, "22 * 44 + 66, *3")
-        .unwrap();
-    assert_eq!(&format!("{:?}", expr), "[((22 * 44) + 66), (error * 3)]");
-
-    let expr = calculator6::ExprsParser::new()
-        .parse(&mut errors, "*")
-        .unwrap();
-    assert_eq!(&format!("{:?}", expr), "[(error * error)]");
-
-    assert_eq!(errors.len(), 4);
+fn header() {
+    assert!(header::HeaderParser::new().parse("/* This is a comment */").is_ok());
+    assert!(header::HeaderParser::new().parse("/* This is a comment, too */").is_ok());
+    assert!(header::HeaderParser::new().parse("/* This is also\n\t a comment, too */").is_ok());
+    assert!(header::HeaderParser::new().parse("/* This comment doesn't end /").is_err());
+    assert!(header::HeaderParser::new().parse("/* This comment doesn't end, either *").is_err());
 }
+//
+// #[test]
+// fn calculator6() {
+//     let mut errors = Vec::new();
+//
+//     let expr = calculator6::ExprsParser::new()
+//         .parse(&mut errors, "22 * + 3")
+//         .unwrap();
+//     assert_eq!(&format!("{:?}", expr), "[((22 * error) + 3)]");
+//
+//     let expr = calculator6::ExprsParser::new()
+//         .parse(&mut errors, "22 * 44 + 66, *3")
+//         .unwrap();
+//     assert_eq!(&format!("{:?}", expr), "[((22 * 44) + 66), (error * 3)]");
+//
+//     let expr = calculator6::ExprsParser::new()
+//         .parse(&mut errors, "*")
+//         .unwrap();
+//     assert_eq!(&format!("{:?}", expr), "[(error * error)]");
+//
+//     assert_eq!(errors.len(), 4);
+// }
